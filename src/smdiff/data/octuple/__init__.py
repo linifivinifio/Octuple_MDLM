de72f098b9d52508c399a3dafd4a3d4a5e028d79
lziltener <lziltener@student.ethz.ch>
2026-01-17 15:19:29 +0100

    fix: don't import deleted file in init

--- src/smdiff/data/octuple/__init__.py
+++ src/smdiff/data/octuple/__init__.py
@@ -1,5 +1,5 @@
 """Octuple data module - PyTorch dataset and MIDI encoding for octuple representation."""
-from .dataset import OctupleDataset
+# from .dataset import OctupleDataset
 from .encoding import OctupleEncoding
 
 __all__ = ['OctupleDataset', 'OctupleEncoding']
